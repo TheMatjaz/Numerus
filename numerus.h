07ceffadf4527ee5ce31e71171443d9fdb5f4c55
Matjaž <dev@matjaz.it>
2015-12-13 10:42:08 +0100

    Fix header documentation formatting

--- numerus.h
+++ numerus.h
@@ -16,14 +16,14 @@ const int ROMAN_MAX_VALUE = 3999;
 const int ROMAN_MIN_VALUE = -3999;
 
 /**
- * Roman numeral for value 0 (zero).
+ * Roman numeral of value 0 (zero).
  */
 const char *NULLA = "NULLA";
 
 /**
  * Maximum length of a roman numeral string including the null terminator.
  *
- * The roman numeral "-MMMDCCCLXXXVIII" (value -3888) + '\0' is a string long
+ * The roman numeral `"-MMMDCCCLXXXVIII"` (value: -3888) + `\0` is a string long
  * 16+1 = 17 chars.
  */
 const int ROMAN_MAX_LENGTH = 17;
@@ -32,6 +32,5 @@ const int ROMAN_MAX_LENGTH = 17;
  * String containing a to-be-compiled regex matching any syntactiacally correct
  * roman numeral.
  */
-const char *ROMAN_SYNTAX_REGEX_STRING =
-        "^-?((M{0,3})(CM|CD|D?C{0,3})(XC|XL|L?X{0,3})(IX|IV|V?I{0,3}))$";
+const char *ROMAN_SYNTAX_REGEX_STRING = "^-?((M{0,3})(CM|CD|D?C{0,3})(XC|XL|L?X{0,3})(IX|IV|V?I{0,3}))$";
 
