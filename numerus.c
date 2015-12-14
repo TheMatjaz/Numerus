eb15284811d1210732e63f5589ec0311ec29ba03
Matjaž <dev@matjaz.it>
2015-12-14 22:47:28 +0100

    Mova all constants to .c file and reference them as extern in .h file
    Otherwise it won't compile because of a double definition. This is the
    only way to share those variables (since they are constants) across all
    users of the library, through the header file.

--- numerus.c
+++ numerus.c
@@ -8,9 +8,37 @@
  * - http://stackoverflow.com/a/30816418/5292928
  */
 
-#include "numerus.h"
-#include <regex.h>  /* To match correct roman numeral syntax */
+#include <regex.h>  /* For `regex_t`, match correct roman numeral syntax*/
 #include <stdlib.h> /* For malloc() */
+
+/**
+ * Maximum value as int a roman numeral may have.
+ */
+const int ROMAN_MAX_VALUE = 3999;
+
+/**
+ * Minimum value as int a roman numeral may have.
+ */
+const int ROMAN_MIN_VALUE = -3999;
+
+/**
+ * Roman numeral of value 0 (zero).
+ */
+const char *NULLA = "NULLA";
+
+/**
+ * Maximum length of a roman numeral string including the null terminator.
+ *
+ * The roman numeral `"-MMMDCCCLXXXVIII"` (value: -3888) + `\0` is a string long
+ * 16+1 = 17 chars.
+ */
+const int ROMAN_MAX_LENGTH = 17;
+
+/**
+ * String containing a to-be-compiled regex matching any syntactiacally correct
+ * roman numeral.
+ */
+const char *ROMAN_SYNTAX_REGEX_STRING = "^-?((M{0,3})(CM|CD|D?C{0,3})(XC|XL|L?X{0,3})(IX|IV|V?I{0,3}))$";
 
 /**
  * Compiled regex matching any syntactiacally correct roman numeral.
--- numerus.h
+++ numerus.h
@@ -3,22 +3,20 @@
  * @brief Numerus roman numerals library header
  */
 
-#include <regex.h>  /* For `regex_t` */
-
 /**
  * Maximum value as int a roman numeral may have.
  */
-const int ROMAN_MAX_VALUE = 3999;
+extern const int ROMAN_MAX_VALUE;
 
 /**
  * Minimum value as int a roman numeral may have.
  */
-const int ROMAN_MIN_VALUE = -3999;
+extern const int ROMAN_MIN_VALUE;
 
 /**
  * Roman numeral of value 0 (zero).
  */
-const char *NULLA = "NULLA";
+extern const char *NULLA;
 
 /**
  * Maximum length of a roman numeral string including the null terminator.
@@ -26,11 +24,13 @@ const char *NULLA = "NULLA";
  * The roman numeral `"-MMMDCCCLXXXVIII"` (value: -3888) + `\0` is a string long
  * 16+1 = 17 chars.
  */
-const int ROMAN_MAX_LENGTH = 17;
+extern const int ROMAN_MAX_LENGTH;
 
 /**
- * String containing a to-be-compiled regex matching any syntactiacally correct
+ * String containing a to-be-compiled regex matching any syntactically correct
  * roman numeral.
  */
-const char *ROMAN_SYNTAX_REGEX_STRING = "^-?((M{0,3})(CM|CD|D?C{0,3})(XC|XL|L?X{0,3})(IX|IV|V?I{0,3}))$";
+extern const char *ROMAN_SYNTAX_REGEX_STRING;
 
+int init();
+int is_roman(char *roman);
