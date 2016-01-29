e6a113e72a0022ba70f389d61c031e1ea7bba117
Matjaž <dev@matjaz.it>
2016-01-29 21:04:18 +0100

    Fix sign macro to return positive sign for 0

--- numerus_internal.h
+++ numerus_internal.h
@@ -2,5 +2,5 @@
 #include "numerus_test.h"
 
 short _num_is_zero(char *roman);
-#define SIGN(x)              (((x) > 0)  - ((x) < 0))
+#define SIGN(x)              (((x) >= 0)  - ((x) < 0))
 #define ABS(x)               (((x) < 0) ? -(x) : (x))
