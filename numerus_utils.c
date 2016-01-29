78fabe7d6cd7f25d13ca3b866334a4834d310118
Matjaž <dev@matjaz.it>
2016-01-29 21:05:56 +0100

    Simplify double to part conversion, collapse in one function

--- numerus_utils.c
+++ numerus_utils.c
@@ -459,14 +459,6 @@ const char *numerus_explain_error(int error_code) {
 }
 
 
-/* pass it values in [0, 1[ to round to the nearest twelfth. Returns the numerator from 0 to 11 */
-static short _num_extract_twelfth(double value) {
-    value = round(value * 12) / 12; /* Round to nearest twelfth */
-    value = round(value * 12); /* Get numerator of that twelfth */
-    return (short) ABS(value);
-}
-
-
 double numerus_parts_to_double(long int_part, short frac_part) {
     return (double) (int_part) + frac_part/12.0;
 }
@@ -476,8 +468,10 @@ long numerus_double_to_parts(double value, short *frac_part) {
     if (frac_part == NULL) {
         frac_part = &zero_frac_part;
     }
-    double double_int_part;
-    double double_frac_part = modf(value, &double_int_part);
-    *frac_part = _num_extract_twelfth(double_frac_part);
-    return (long) double_int_part;;
+    long int_part = (long) floor(value);
+    value -= int_part; /* Get only decimal part */
+    value = round(value * 12) / 12; /* Round to nearest twelfth */
+    value = round(value * 12); /* Get numerator of that twelfth */
+    *frac_part = (short) value;
+    return int_part;
 }
