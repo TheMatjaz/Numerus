727a262d7ad70da47481e0fd2f68fd08e740b88d
Matjaž <dev@matjaz.it>
2018-05-13 22:58:58 +0200

    Add Clion project folder with code styles and templates

---
+++ .idea/fileTemplates/internal/C Header File.h
@@ -0,0 +1,16 @@
+#parse("C File Header.h")
+#[[#ifndef]]# ${INCLUDE_GUARD}
+#[[#define]]# ${INCLUDE_GUARD}
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#[[#endif]]# //${INCLUDE_GUARD}
