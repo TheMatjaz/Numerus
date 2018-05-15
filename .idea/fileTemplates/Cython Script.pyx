063eef6cde25ca76a70124f772b3da143adbe94f
Matjaž <dev@matjaz.it>
2018-05-15 13:19:11 +0200

    Add file templates for Python and Cython

---
+++ .idea/fileTemplates/Cython Script.pyx
@@ -0,0 +1,3 @@
+#!/usr/bin/env cython
+# -*- coding: utf8 -*-
+
---
+++ .idea/fileTemplates/internal/Setup Script.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf8 -*-
+
+$Import
+
+setup(
+    name='$Package_name',
+    version='$Version',
+    packages=$PackageList,$PackageDirs
+    url='$URL',
+    license='$License',
+    author='$Author',
+    author_email='$Author_Email',
+    description='$Description'
+)
