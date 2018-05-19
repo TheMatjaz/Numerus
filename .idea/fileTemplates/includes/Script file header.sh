2c43853703f49eed2be0b879ac8de34065a68671
Matjaž <dev@matjaz.it>
2018-05-19 11:30:56 +0200

    Fix the hardcoded title of the script file template header

--- .idea/fileTemplates/includes/Script file header.sh
+++ .idea/fileTemplates/includes/Script file header.sh
@@ -1,5 +1,5 @@
 # -----------------------------------------------------------------------------
-# Numerus CMake main configuration file.
+# ${Title}
 # Copyright © 2015-${YEAR}, Matjaž Guštin <dev@matjaz.it> https://matjaz.it
 # All rights reserved.
 # Released under the BSD 3-clause license.
