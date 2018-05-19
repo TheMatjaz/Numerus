3f56c3199b62045209036a289bed8f7ca9e939a3
Matjaž <dev@matjaz.it>
2018-05-19 11:31:24 +0200

    Improve titles of the CMake files (in the file headers)

--- cmake/build_type.cmake
+++ cmake/build_type.cmake
@@ -1,5 +1,5 @@
 # -----------------------------------------------------------------------------
-# Numerus CMake main configuration file.
+# Default build type and debug C macro CMake configuration file.
 # Copyright © 2015-2018, Matjaž Guštin <dev@matjaz.it> https://matjaz.it
 # All rights reserved.
 # Released under the BSD 3-clause license.
--- cmake/compiler_flags.cmake
+++ cmake/compiler_flags.cmake
@@ -1,5 +1,5 @@
 # -----------------------------------------------------------------------------
-# Numerus CMake main configuration file.
+# Compile flags and warnings CMake configuration file.
 # Copyright © 2015-2018, Matjaž Guštin <dev@matjaz.it> https://matjaz.it
 # All rights reserved.
 # Released under the BSD 3-clause license.
