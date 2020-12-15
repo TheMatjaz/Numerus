71419a5be8b62cd5f1af021676728226dca3e1f4
Matjaž <dev@matjaz.it>
2020-12-15 19:50:08 +0100

    Remove @brief from Doxygen file header template

--- .idea/fileTemplates/includes/Doxygen file header.h
+++ .idea/fileTemplates/includes/Doxygen file header.h
@@ -1,6 +1,6 @@
 /**
  * @file
- * @brief 
+ * 
  * @copyright Copyright © 2015-${YEAR}, Matjaž Guštin <dev@matjaz.it>
  * <https://matjaz.it>. All rights reserved.
  * @license BSD 3-clause license.
