d03266c103ae806ee088590451d98cb913bc7126
Matjaž <dev@matjaz.it>
2020-12-15 19:18:14 +0100

    Basic Cmake targets with test framework skeleton

---
+++ tst/main.c
@@ -0,0 +1,16 @@
+/**
+ * @file
+ *
+ * @copyright Copyright © 2015-2020, Matjaž Guštin <dev@matjaz.it>
+ * <https://matjaz.it>. All rights reserved.
+ * @license BSD 3-clause license.
+ */
+
+#include "numerus_test.h"
+
+
+int main(void)
+{
+    puts("No tests implemented!");
+    return atto_at_least_one_fail;
+}
---
+++ tst/numerus_test.h
@@ -0,0 +1,24 @@
+/**
+ * @file
+ * @brief
+ * @copyright Copyright © 2015-2020, Matjaž Guštin <dev@matjaz.it>
+ * <https://matjaz.it>. All rights reserved.
+ * @license BSD 3-clause license.
+ */
+
+#ifndef NUMERUS_TEST_H_
+#define NUMERUS_TEST_H_
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+#include "atto.h"
+#include "numerus.h"
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif  /* NUMERUS_TEST_H_ */
